d3baa4e78b2d45a1cbb36293f46b894699b99a16
Alfred Carra <go8210@wayne.edu>
2022-12-04 23:15:58 -0500

    adding hot user class
    necessary for file descriptors

---
+++ src/hotUserDAO.java
@@ -0,0 +1,27 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.sql.PreparedStatement;
+//import java.sql.Connection;
+//import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+//import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class hotUserDAO {
+
+}
